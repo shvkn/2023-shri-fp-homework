4136cdd5c60c4e006c20d6277a72e7f356362549
Anatoly Shuvakhin <a.shuvakhin@gmail.com>
2023-07-20 09:28:39 +0700

    refactor(processSequence): fp 2

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,77 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from '../tools/api';
+import * as R from "ramda";
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const lengthGtTwo = R.compose(
+    R.gt(R.__, 2),
+    R.length
+);
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const lengthLtTen = R.compose(
+    R.lt(R.__, 10),
+    R.length
+);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const validate = R.allPass([
+    lengthGtTwo,
+    lengthLtTen,
+    R.test(/^\d+(\.\d+)?$/)
+]);
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const convertToNumber = (str) => Number(str);
+const round = (val) => Math.round(val);
+const square = (val) => val ** 2;
+const remDivOn3 = (val) => val % 3;
+const assocNumber = R.assoc('number', R.__, {from: 10, to: 2});
+const getResult = R.prop('result')
+const fetchBinary = R.pipe(
+    assocNumber,
+    api.get('https://api.tech/numbers/base'),
+    R.andThen(
+        getResult
+    )
+);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const animalUrl = (id) => `https://animals.tech/${id}`;
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const fetchAnimalById = R.pipe(
+    animalUrl,
+    api.get(R.__, {}),
+    R.andThen(
+        getResult
+    )
+);
+
+const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
+    const handleLog = R.tap(writeLog);
+    const handleValidationError = R.partial(
+        handleError,
+        ['ValidationError']
+    );
+    const processFlow = R.pipe(
+        handleLog,
+        convertToNumber,
+        round,
+        handleLog,
+        fetchBinary,
+        R.andThen(handleLog),
+        R.otherwise(handleError),
+        R.andThen(R.length),
+        R.andThen(handleLog),
+        R.andThen(square),
+        R.andThen(handleLog),
+        R.andThen(remDivOn3),
+        R.andThen(handleLog),
+        R.andThen(fetchAnimalById),
+        R.andThen(handleSuccess),
+        R.otherwise(handleError),
+    );
+    const validateAndProcess = R.ifElse(validate, processFlow, handleValidationError)
+    validateAndProcess(value);
+}
 
 export default processSequence;
