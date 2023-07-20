3bc7a688fb31100eeaae34b1c89a6655d9d57bdc
Anatoly Shuvakhin <a.shuvakhin@gmail.com>
2023-07-20 14:57:40 +0700

    refactor: imports

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -12,57 +12,57 @@
  *
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
-import {__, allPass, any, complement, compose, count, countBy, equals, filter, gte, prop, toLower, values} from "ramda";
+import * as R from "ramda";
 
-const isStar = prop('star')
-const isSquare = prop('square')
-const isTriangle = prop('triangle')
-const isCircle = prop('circle')
+const isStar = R.prop('star')
+const isSquare = R.prop('square')
+const isTriangle = R.prop('triangle')
+const isCircle = R.prop('circle')
 
-const isRed = equals('red')
-const isGreen = equals('green')
-const isWhite = equals('white')
-const isBlue = equals('blue')
-const isOrange = equals('orange');
+const isRed = R.equals('red')
+const isGreen = R.equals('green')
+const isWhite = R.equals('white')
+const isBlue = R.equals('blue')
+const isOrange = R.equals('orange');
 
-const isRedStar = compose(isRed, isStar);
-const isGreenSquare = compose(isGreen, isSquare);
-const isWhiteTriangle = compose(isWhite, isTriangle);
-const isWhiteCircle = compose(isWhite, isCircle);
-const isBlueCircle = compose(isBlue, isCircle);
-const isOrangeSquare = compose(isOrange, isSquare);
-const isGreenTriangle = compose(isGreen, isTriangle);
+const isRedStar = R.compose(isRed, isStar);
+const isGreenSquare = R.compose(isGreen, isSquare);
+const isWhiteTriangle = R.compose(isWhite, isTriangle);
+const isWhiteCircle = R.compose(isWhite, isCircle);
+const isBlueCircle = R.compose(isBlue, isCircle);
+const isOrangeSquare = R.compose(isOrange, isSquare);
+const isGreenTriangle = R.compose(isGreen, isTriangle);
 
-const countOfGreens = compose(count(isGreen), values)
-const countOfOranges = compose(count(isOrange), values);
-const countOfReds = compose(count(isRed), values);
-const countOfBlues = compose(count(isBlue), values);
+const countOfGreens = R.compose(R.count(isGreen), R.values)
+const countOfOranges = R.compose(R.count(isOrange), R.values);
+const countOfReds = R.compose(R.count(isRed), R.values);
+const countOfBlues = R.compose(R.count(isBlue), R.values);
 
-const isNotWhite = complement(isWhite);
-const isNotRed = complement(isRed);
+const isNotWhite = R.complement(isWhite);
+const isNotRed = R.complement(isRed);
 
-const isNotRedStar = compose(isNotRed, isStar);
-const isNotWhiteStar = compose(isNotWhite, isStar);
-const isNotWhiteTriangle = complement(isWhiteTriangle);
+const isNotRedStar = R.compose(isNotRed, isStar);
+const isNotWhiteStar = R.compose(isNotWhite, isStar);
+const isNotWhiteTriangle = R.complement(isWhiteTriangle);
 
 const isTriangleEqualsSquare = ({triangle, square}) => triangle === square;
 
-const threeOfThemHaveSameColor = compose(
-    any(gte(__, 3)),
-    values,
-    countBy(toLower),
+const threeOfThemHaveSameColor = R.compose(
+    R.any(R.gte(R.__, 3)),
+    R.values,
+    R.countBy(R.toLower),
 );
 
-const notWhiteFigures = compose(
-    filter(isNotWhite),
-    values
+const notWhiteFigures = R.compose(
+    R.filter(isNotWhite),
+    R.values
 );
 
-const isThereTwoGreens = compose(equals(2), countOfGreens);
-const isThereOneRed = compose(equals(1), countOfReds)
+const isThereTwoGreens = R.compose(R.equals(2), countOfGreens);
+const isThereOneRed = R.compose(R.equals(1), countOfReds)
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = allPass([
+export const validateFieldN1 = R.allPass([
     isRedStar,
     isGreenSquare,
     isWhiteTriangle,
@@ -70,58 +70,59 @@ export const validateFieldN1 = allPass([
 ]);
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = compose(
-    gte(__, 2),
+export const validateFieldN2 = R.compose(
+    R.gte(R.__, 2),
     countOfGreens
-)
+);
+
 // 3. Количество красных фигур равно кол-ву синих.
 export const validateFieldN3 = (figures) => {
-    return equals(
-        countOfReds(figures),
-        countOfBlues(figures)
-    );
+    return R.equals(
+        countOfBlues(figures),
+        countOfReds(figures)
+    )
 }
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = allPass([
+export const validateFieldN4 = R.allPass([
     isBlueCircle,
     isRedStar,
     isOrangeSquare,
 ]);
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = compose(
+export const validateFieldN5 = R.compose(
     threeOfThemHaveSameColor,
     notWhiteFigures
 );
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = allPass([
+export const validateFieldN6 = R.allPass([
     isThereOneRed,
     isThereTwoGreens,
     isGreenTriangle
 ]);
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = compose(
-    gte(__, 4),
+export const validateFieldN7 = R.compose(
+    R.gte(R.__, 4),
     countOfOranges
 );
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = allPass([
+export const validateFieldN8 = R.allPass([
     isNotRedStar,
     isNotWhiteStar
 ]);
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = compose(
-    gte(__, 4),
+export const validateFieldN9 = R.compose(
+    R.gte(R.__, 4),
     countOfGreens
 );
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = allPass([
+export const validateFieldN10 = R.allPass([
     isNotWhiteTriangle,
     isTriangleEqualsSquare
 ]);
